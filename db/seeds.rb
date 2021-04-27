2df26af1145d06cc1e677a83ec8fa809598711be
Malcolm Jeremiah Rajeli <malcolmjeremiahrajeli@gmail.com>
2021-04-27 10:28:05 +0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+def seed_post
+  10.times do
+    post = Post.create!(title: Faker::Lorem.words.join(" ").titleize, content: Faker::Lorem.paragraph(sentence_count: (3..10).to_a.sample))
+    seed_comments(post)
+  end
+end
+
+def seed_comments(post)
+  10.times do
+    post.comments.create!(content: Faker::Lorem.paragraph(sentence_count: (3..10).to_a.sample))
+  end
+end
+
+seed_post
